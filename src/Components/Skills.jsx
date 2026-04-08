46291539ac60b0c8f461b191e7a4cdc462f7c4ac
PreethikaSubramani <preethikasubramani02@gmail.com>
2026-04-08 14:44:57 +0100

    message

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -11,22 +11,25 @@ function Skills() {
       <h2>Skills</h2>
       <div className="frontEnd">
         <h1>  FrontEnd Development</h1>
-        <h3>JavaScript, TypeScript, SQL, HTML5, CSS3, python</h3>
+        <h3><span>JavaScript</span> <span>TypeScript</span> <span>SQL</span> <span>SQL</span>
+          <span>HTML5</span> <span>CSS3</span> <span>Python</span> </h3>
       </div>
 
       <div className="framework">
         <h1> Framework</h1> 
-        <h3> React, Redux, Tailwind css, Vite </h3>
+        <h3> <span>React</span> <span>Redux</span><span>TailWind css</span><span>Vite</span></h3>
       </div>
 
       <div className="others">
         <h1>Version control / API</h1>
-        <h3>Git, GitHub, RestAPI, JSON, Postman, CRUD</h3>
+        <h3><span>Git</span> <span>GitHub</span> <span>RestAPI</span> <span>JSON</span> 
+        <span>Postman</span> <span>CRUC</span></h3>
       </div>
 
       <div className="technical">
         <h1>Technical Skills</h1>
-        <h3>Agile, Scrum, Jira, Collaboration, Confluence</h3>
+        <h3><span>Agile</span> <span>Scrum</span> <span>Jira</span> <span>Collaboration</span>
+        <span>Confluence</span></h3>
       </div>
     </Element>
   );
